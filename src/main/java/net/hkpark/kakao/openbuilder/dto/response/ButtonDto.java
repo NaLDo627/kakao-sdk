745180ab03a1b37a620180171e47d19ae83f0a54
NaLDo627 <naruhodo627@gmail.com>
2020-09-30 16:50:06 +0900

    fix: JsonInclude 추가

--- src/main/java/net/hkpark/kakao/openbuilder/dto/response/ButtonDto.java
+++ src/main/java/net/hkpark/kakao/openbuilder/dto/response/ButtonDto.java
@@ -1,5 +1,6 @@
 package net.hkpark.kakao.openbuilder.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Getter;
@@ -28,6 +29,7 @@ public class ButtonDto {
      * 웹 브라우저를 열고 webLinkUrl 의 주소로 이동합니다
      */
     @JsonProperty("webLinkUrl")
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private final String webLinkUrl;
 
     /**
@@ -35,18 +37,21 @@ public class ButtonDto {
      * block: 블록 연결시 사용자의 발화로 노출됩니다.
      */
     @JsonProperty("messageText")
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private final String messageText;
 
     /**
      * phoneNumber에 있는 번호로 전화를 겁니다.
      */
     @JsonProperty("phoneNumber")
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private final String phoneNumber;
 
     /**
      * blockId를 갖는 블록을 호출합니다. (바로가기 응답의 블록 연결 기능과 동일)
      */
     @JsonProperty("blockId")
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private final String blockId;
 
     /**
@@ -54,5 +59,6 @@ public class ButtonDto {
      */
     @Builder.Default
     @JsonProperty("extra")
+    @JsonInclude(JsonInclude.Include.NON_EMPTY)
     private final Map<String, Object> extra = new HashMap<>();
 }
--- src/main/java/net/hkpark/kakao/openbuilder/dto/response/QuickReplyDto.java
+++ src/main/java/net/hkpark/kakao/openbuilder/dto/response/QuickReplyDto.java
@@ -1,8 +1,12 @@
 package net.hkpark.kakao.openbuilder.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.experimental.SuperBuilder;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 바로가기 응답은 발화와 동일합니다. 대신, 사용자가 직접 발화를 입력하지 않아도 선택을 통해서 발화를 전달하거나 다른 블록을 호출할 수 있습니다.
@@ -33,12 +37,14 @@ public class QuickReplyDto {
      * 바로가기 응답이 '블록연결' 기능일 때, 연결될 블록의 id
      * action이 'block'일 때, 필수값
      */
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     @JsonProperty("blockId")
     private final String blockId;
 
     /**
      * 블록을 호출 시 추가적으로 제공하는 정보
      */
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     @JsonProperty("extra")
-    private final Object extra;
+    private final Map<String, Object> extra = new HashMap<>();
 }
