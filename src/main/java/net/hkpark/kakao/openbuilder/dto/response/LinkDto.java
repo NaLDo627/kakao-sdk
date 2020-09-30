c26c8218cb2dd8052039d04405efd9682929c81d
NaLDo627 <naruhodo627@gmail.com>
2020-09-30 13:20:01 +0900

    feat: maven central 저장소 사용

--- src/main/java/net/hkpark/kakao/openbuilder/dto/response/LinkDto.java
+++ src/main/java/net/hkpark/kakao/openbuilder/dto/response/LinkDto.java
@@ -7,8 +7,8 @@ import lombok.experimental.SuperBuilder;
 /**
  * Information. 링크 우선순위 링크는 다음과 같은 우선순위를 갖습니다.
  *
- * pc: pc < web
- * 모바일: mobile < web
+ * pc: pc &lt; web
+ * 모바일: mobile &lt; web
  * 예를 들면, pc에 대하여 링크 값이 webURL, pcURL를 가지면 위 규칙에 따라 webURL이 노출됩니다.
  * 모바일 기기에 대하여 Link의 값이 webURL, mobileURL를 가지면 위 규칙에 따라 webURL이 노출됩니다.
  */
